4a29a2b46f5b0d50d2a2642f68a3befc215c9b84
sixelasacul <pro@sixe.la>
2026-08-03 16:39:06 +0200

    initial commit

---
+++ run_onchange_install-packages.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+brew install nushell
+
+flatpak install flathub \
+	app.zen_browser.zen \
+	dev.zed.Zed \
+	com.bitwarden.desktop \
+	org.localsend.localsend_app \
+	md.obsidian.Obsidian \
+	com.nextcloud.desktopclient.nextcloud \
+	com.blitzfc.qbz \
+	dev.vencord.Vesktop
