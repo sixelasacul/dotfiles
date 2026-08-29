949480ac8e6579de8b4f3d7a0391909f8c46f9fa
sixelasacul <pro@sixe.la>
2026-08-29 16:01:27 +0200

    added fish config

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,9 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+set -gx EDITOR "zed --wait"
